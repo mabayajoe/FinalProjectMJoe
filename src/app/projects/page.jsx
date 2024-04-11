d2c8f37c9bc1433ca35d08548922d0295eb30a86
JoeM. <75545397+mabayajoe@users.noreply.github.com>
2024-04-11 16:57:06 +0000

    ..

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Navebar from "@/components/menu/HomeB.jsx";
 import Footer from "@/components/menu/footer.jsx";
 import "./projects.css";
 import logo from "./projectPic.png";
+
 const links = [
   {
     title: "Home",
@@ -22,95 +22,21 @@ const links = [
     url: "/uses",
   },
 ];
+
 function ProjectCard({ logos, name, content, link }) {
   return (
-    <div className="relative">
-      <div className="flex">
-        <div>
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-
-          <div style={{ marginBottom: "0.5cm" }}> </div>
-
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-        </div>
-
-        <br></br>
-        <div style={{ marginLeft: "0.5cm" }}>
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-
-          <div style={{ marginBottom: "0.5cm" }}> </div>
-
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-        </div>
-
-        <br></br>
-        {/*third column */}
-        <div style={{ marginLeft: "0.5cm", marginBottom: "2cm" }}>
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-
-          <div style={{ marginBottom: "0.5cm" }}> </div>
-
-          <div data-testid="projectCard" className="project-card">
-            <img data-testid="projectCardLogo" src={logos} alt={`${name} Logo`} className="project-card-logo" />
-            <h2 data-testid="projectCardName">{name}</h2>
-
-            <p data-testid="projectCardContent">{content}</p>
-            <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer">
-              <img src="./link.svg" alt="" /> View Project
-            </a>
-          </div>
-        </div>
-
-        <br></br>
-
-        <br></br>
-      </div>
+    <div className="project-card">
+      <img src={logos} alt={`${name} Logo`} className="project-card-logo" />
+      <h2>{name}</h2>
+      <p>{content}</p>
+      <a href={link} target="_blank" rel="noreferrer">
+        <img src="./link.svg" alt="" /> View Project
+      </a>
     </div>
   );
 }
 
 export default function Projects() {
-  //Return HTML elements for Project page
   return (
     <div className="body">
       <div className="navebar">
@@ -119,20 +45,53 @@ export default function Projects() {
 
       <div className="projectBody">
         <h1 className="h1">Things I’ve made trying to put my dent in the universe.</h1>
-        <br></br>
+        <br />
         <p>
           Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
           adipiscing elit Ut et massa mi..
         </p>
-        <br></br>
+        <br />
       </div>
 
-      <ProjectCard
-        name="Calculator"
-        logos={"./projectPic.png"}
-        content={"Creating technology to empower civilians to explore space on their own terms."}
-        link={"https://stage.company-corp.com/platform"}
-      />
+      <div className="project-cards">
+        <ProjectCard
+          name="Calculator"
+          logos={"./projectPic.png"}
+          content={"Creating technology to empower civilians to explore space on their own terms."}
+          link={"https://stage.company-corp.com/platform"}
+        />
+        <ProjectCard
+          name="Real Estate App"
+          app
+          logos={"./projectPic2.png"}
+          content={"Description of Project 2."}
+          link={"https://example.com/project2"}
+        />
+        <ProjectCard
+          name="Project 3"
+          logos={"./projectPic3.png"}
+          content={"Description of Project 3."}
+          link={"https://example.com/project3"}
+        />
+        <ProjectCard
+          name="Project 4"
+          logos={"./projectPic4.png"}
+          content={"Description of Project 4."}
+          link={"https://example.com/project4"}
+        />
+        <ProjectCard
+          name="Project 5"
+          logos={"./projectPic5.png"}
+          content={"Description of Project 5."}
+          link={"https://example.com/project5"}
+        />
+        <ProjectCard
+          name="Project 6"
+          logos={"./projectPic6.png"}
+          content={"Description of Project 6."}
+          link={"https://example.com/project6"}
+        />
+      </div>
 
       <div className="footerBox">
         <Footer className="Footer" links={links} />
